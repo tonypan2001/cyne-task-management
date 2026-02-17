f1d2f9224c2a29fee252d2c190019fc34a95a6bb
Prompan Uechanwech <ueprompan@central.co.th>
2026-02-17 22:54:17 +0700

    add fonts to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,21 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // บังคับให้ระบบมองหา class "dark" เท่านั้นถึงจะเปลี่ยนสี 
-  // ซึ่งถ้าเราไม่ใส่ class นี้ไว้ที่ <html> มันก็จะขาวสะอาดตลอดไปค่ะ
-  darkMode: 'class', 
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    // บังคับให้ระบบมองหา class "dark" เท่านั้นถึงจะเปลี่ยนสี 
+    // ซึ่งถ้าเราไม่ใส่ class นี้ไว้ที่ <html> มันก็จะขาวสะอาดตลอดไปค่ะ
+    darkMode: 'class',
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['var(--font-line-seed)', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
 };
 export default config;
