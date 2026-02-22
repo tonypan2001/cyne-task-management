57e702b4d69907e838a3bcbf5f1c5afbd382a5f8
Prompan Uechanwech <panzaza9543@gmail.com>
2026-02-22 12:02:38 +0700

    rename menu

--- constants/menu.ts
+++ constants/menu.ts
@@ -2,7 +2,7 @@ import { CheckCircle2, LayoutDashboard, PlusSquare, User } from "lucide-react";
 
 export const menuItems = [
     { name: 'Dashboard', path: '/', icon: LayoutDashboard },
-    { name: 'สร้างงานใหม่', path: '/create', icon: PlusSquare },
-    { name: 'งานที่เสร็จแล้ว', path: '/completed', icon: CheckCircle2 },
+    { name: 'New Task', path: '/create', icon: PlusSquare },
+    { name: 'Completed Task', path: '/completed', icon: CheckCircle2 },
     { name: 'Team', path: '/team', icon: User},
 ]
