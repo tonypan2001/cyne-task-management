e4eb5bbfaee9bf55068375a7d77f607bcc28f0a4
Prompan Uechanwech <panzaza9543@gmail.com>
2026-02-22 12:06:32 +0700

    rename menu and change icon menu

--- constants/menu.ts
+++ constants/menu.ts
@@ -1,8 +1,8 @@
-import { CheckCircle2, LayoutDashboard, PlusSquare, User } from "lucide-react";
+import { CheckCircle2, LayoutDashboard, PlusSquare, Users } from "lucide-react";
 
 export const menuItems = [
     { name: 'Dashboard', path: '/', icon: LayoutDashboard },
     { name: 'New Task', path: '/create', icon: PlusSquare },
-    { name: 'Completed Task', path: '/completed', icon: CheckCircle2 },
-    { name: 'Team', path: '/team', icon: User},
+    { name: 'Completed', path: '/completed', icon: CheckCircle2 }, // หรือคงไว้เป็น 'Completed Task' ก็ได้ค๊ะ
+    { name: 'Team', path: '/team', icon: Users }, // ✨ เปลี่ยนไอคอนเป็น Users ค๊ะ
 ]
